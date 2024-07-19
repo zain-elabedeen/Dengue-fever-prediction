c8dcc1934ed694056455d5d4f94e8832b56c8ee5
Zain <zain@zains-mbp.speedport_w_724v_typ_a_05011603_06_003>
2024-07-19 17:00:28 +0200

    Train model

--- src/dengue_fever_prediction/pipelines/data_processing/nodes.py
+++ src/dengue_fever_prediction/pipelines/data_processing/nodes.py
@@ -5,10 +5,12 @@ import pandas as pd
 # from pyspark.sql import DataFrame as SparkDataFrame
 # from pyspark.sql.functions import regexp_replace
 # from pyspark.sql.types import DoubleType
-#from preprocessing_node import preprocessing
+# from preprocessing_node import preprocessing
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
 
 
-def merge_data(dengue_features_test: pd.DataFrame, dengue_features_train: pd.DataFrame, dengue_labels_train: pd.DataFrame) -> pd.DataFrame: 
+def merge_data(dengue_features_test: pd.DataFrame, dengue_features_train: pd.DataFrame, dengue_labels_train: pd.DataFrame) -> pd.DataFrame:
     dengue_features_test.insert(0, 'type', 'test')
     dengue_features_train.insert(0, 'type', 'train')
     column_to_merge = dengue_labels_train[['total_cases']]
@@ -22,11 +24,11 @@ def merge_data(dengue_features_test: pd.DataFrame, dengue_features_train: pd.Dat
 
 def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Wrapper function for doing the preprocessing steps: 
+    Wrapper function for doing the preprocessing steps:
         1) Handling the date column
         2) Imputation of missing values
         3) One-Hot encoding of the city parameter
-    
+
     Input: Big DF beast from Arpad
 
     Output: Big DF that has only numerical values.
@@ -35,17 +37,25 @@ def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
     # 1) Handling the Date:
     df = changeDate(df) # TODO: best way to pass a dataframe?
 
-    # 2) Imputation: 
+    # 2) Imputation:
     df = impute(df)
 
-    # 3) One-hot encoding: 
+    # 3) One-hot encoding:
     df = encode(df)
 
-    return df 
+    return df
 
 
-def fit_model():
-    return {}
+def train_model(preprocessed_data: pd.DataFrame, parameters: Dict) -> RandomForestRegressor:
+    training_data = preprocessed_data[preprocessed_data['type_train'] == 1]
+
+    X = training_data[parameters["features"]]
+    y = training_data["total_cases"]
+
+
+    regressor = RandomForestRegressor()
+    regressor.fit(X, y)
+    return regressor
 
 def prediction():
     return {}
@@ -55,13 +65,13 @@ def submittion():
 
 
 
-# Helper functions: 
+# Helper functions:
 
 def changeDate(df: pd.DataFrame) -> pd.DataFrame:
     """
     For now, just remove it. (TODO)
     """
-    
+
     df.drop('week_start_date', axis=1, inplace=True)
 
     return df
@@ -71,15 +81,15 @@ def impute(df: pd.DataFrame) -> pd.DataFrame:
     """
     Following the benchmark example: Filling in whatever
     """
-    # TODO: Gotta think about the \hat{y} predicted values here? 
+    # TODO: Gotta think about the \hat{y} predicted values here?
     df.fillna(method='ffill', inplace=True)
 
-    return df 
+    return df
 
 
 def encode(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Using one-hot encoding for the city column. 
+    Using one-hot encoding for the city column.
 
     New columns to df: "sj", "iq" = 0 or 1
     """
--- src/dengue_fever_prediction/pipelines/data_processing/pipeline.py
+++ src/dengue_fever_prediction/pipelines/data_processing/pipeline.py
@@ -3,7 +3,7 @@ from kedro.pipeline import Pipeline, node, pipeline
 from .nodes import (
     merge_data,
     preprocess_data,
-    fit_model,
+    train_model,
     prediction,
     submittion,
 )
@@ -14,7 +14,7 @@ def create_pipeline(**kwargs) -> Pipeline:
             node(
                 func=merge_data,
                 inputs=["dengue_features_train", "dengue_features_test", "dengue_labels_train"],
-                outputs= "merged_data",
+                outputs="merged_data",
                 name="merge_data_node",
             ),
             node(
@@ -23,12 +23,12 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="preprocessed_data",
                 name="preprocess_data_node",
             ),
-            # node(
-            #     func=fit_model,
-            #     inputs=["preprocessed_data"],
-            #     outputs="fitted_model_data",
-            #     name="fit_model_node"
-            # ),
+            node(
+                func=train_model,
+                inputs=["preprocessed_data", "params:model_options"],
+                outputs="trained_model",
+                name="train_model_node"
+            ),
             # node(
             #     func=prediction,
             #     inputs=["fitted_model_data"],
